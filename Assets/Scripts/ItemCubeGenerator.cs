de8a85064db9a5858b1451f6fbd86afee3043ec9
Nyarll <oss314y08@i.softbank.jp>
2020-06-23 12:49:25 +0900

    update : Random Generate ItemBox

---
+++ Assets/Scripts/ItemCubeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCubeGenerator : MonoBehaviour
+{
+    public GameObject item;
+    public int num = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < num; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-18, 18), 2, Random.Range(-18, 18));
+            GameObject instance = (GameObject)Instantiate(item, pos, Quaternion.identity, transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -16,7 +16,7 @@ public class Pickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetKeyDown(KeyCode.E))
         {
             if(curObject == null)
             {
