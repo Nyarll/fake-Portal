616a716f354c0b41b3321a318ba1a980baab2027
Nyarll <oss314y08@i.softbank.jp>
2020-06-23 12:29:26 +0900

    update : Carry Object

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    GameObject curObject;
+    Rigidbody curBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(curObject == null)
+            {
+                pickupItem();
+            }
+            else
+            {
+                dropItem();
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(curObject != null)
+        {
+            reposObject();
+        }
+    }
+
+    void pickupItem()
+    {
+        // <正面へrayを飛ばす>
+        float angleDir = transform.eulerAngles.y * (Mathf.PI / 180.0f);
+        Vector3 dir1 = new Vector3(Mathf.Sin(angleDir), 0, Mathf.Cos(angleDir));
+        RaycastHit hitObject;
+
+        if (Physics.Raycast(transform.position, dir1, out hitObject))
+        {
+            if (hitObject.transform.tag == "Item")
+            {
+                curObject = hitObject.collider.gameObject;
+                hitObject.rigidbody.useGravity = false;
+            }
+        }
+    }
+
+    void dropItem()
+    {
+        curObject.GetComponent<Rigidbody>().useGravity = true;
+        curObject = null;
+    }
+
+    void reposObject()
+    {
+        Transform child = transform.GetChild(1).transform;
+        curObject.transform.position = child.position;
+        curObject.transform.rotation = transform.rotation;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject camera;
+    public float speed;
+    private Transform playerTransform;
+    private Transform cameraTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = transform.parent;
+        cameraTransform = GetComponent<Transform>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xRot = Input.GetAxis("Mouse X");
+        float yRot = Input.GetAxis("Mouse Y");
+
+        playerTransform.transform.Rotate(0, xRot, 0);
+        cameraTransform.transform.Rotate(-yRot, 0, 0);
+
+        float angleDir = playerTransform.transform.eulerAngles.y * (Mathf.PI / 180.0f);
+        Vector3 dir1 = new Vector3(Mathf.Sin(angleDir), 0, Mathf.Cos(angleDir));
+        Vector3 dir2 = new Vector3(-Mathf.Cos(angleDir), 0, Mathf.Sin(angleDir));
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            playerTransform.transform.position += dir1 * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerTransform.transform.position += dir2 * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerTransform.transform.position += -dir2 * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerTransform.transform.position += -dir1 * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+}
